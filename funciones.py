0eed31b2beaa272594e2c29c7a24f2f6577e2503
Oscar Garcia <ogarcia30@estudiantes.areandina.edu.co>
2022-06-29 21:14:37 -0500

    Mi primera funcion de multiplicar, agosto

--- funciones.py
+++ funciones.py
@@ -5,5 +5,19 @@ def nombre(): # mi primera función.
     a=3
     b=4
     c=a+b
-    print(c)
+    print("El resultado de la suma: ",c)
 nombre()
+
+def resta():
+    a=2
+    b=1
+    c=a-b
+    print("El resultado de la resta es: ",c)
+resta()
+
+def multi():
+    a=4
+    b=8
+    c=a*b
+    print("El resultado es la multiplicación es; ",c)
+multi()
